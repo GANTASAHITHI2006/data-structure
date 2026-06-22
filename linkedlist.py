2083b5bad883f45b3aebe75208f3d58f372e831f
Sahithi <sahithiganta7@gmail.com>
2026-06-22 14:30:52 +0530

    code added

---
+++ linkedlist.py
@@ -0,0 +1,9 @@
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class linkedlist:
+    def_init__(self):
+        self.head = None
+    def add_end(self, data):
+        
