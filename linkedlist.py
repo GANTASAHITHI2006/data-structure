3d30429511df3125b0489dfa8111918358baac45
Sahithi <sahithiganta7@gmail.com>
2026-06-22 16:15:47 +0530

    middle ele

--- linkedlist.py
+++ linkedlist.py
@@ -1,35 +1,39 @@
 class Node:
-    def __init__(self,data):
-        self.data = data
+    def __init__(self, value):
+        self.data = value
         self.next = None
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
-    def add_end(self,data):
-        new = Node(data)
+
+    def add_node(self, value):
+        new = Node(value)
+
         if self.head is None:
             self.head = new
-            return
-        itr = self.head
-        while itr.next:
-            itr = itr.next
-        itr.next = new
-    def add_beg(self,data):
-            obj=Node(data)
-            if self.head is None:
-                self.head=obj
-                return 
-            obj.next = self.head
-            self.head=obj
-    def display(self):
-        itr = self.head
-        while itr:
-            print(itr.data,end ="-->")
-            itr = itr.next
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new
+
+    def middle_node(self):
+        p1 = self.head
+        p2 = self.head
+
+        while p2 is not None and p2.next is not None:
+            p1 = p1.next
+            p2 = p2.next.next
+
+        return p1.data
 ll = LinkedList()
-ll.add_end(50)
-ll.add_end(100)
-ll.add_end(200)
-ll.add_end(250)
-ll.add_beg(10)
-ll.display()
+
+n = int(input("How many nodes? "))
+
+for i in range(n):
+    num = int(input("Enter value: "))
+    ll.add_node(num)
+
+print("Middle element is:", ll.middle_node())
